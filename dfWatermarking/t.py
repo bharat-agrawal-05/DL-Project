56f08663f38fcd118466fb1e2fbaa5184c4ea56e
Akshit <akshitbhola2005@gmail.com>
2025-05-28 10:45:35 +0530

    Formatted dfWatermarking

--- dfWatermarking/t.py
+++
@@ -1,9 +0,0 @@
-import torch
-print(f"PyTorch version: {torch.__version__}")
-print(f"CUDA available: {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"PyTorch CUDA version: {torch.version.cuda}")
-    print(f"Number of GPUs: {torch.cuda.device_count()}")
-    print(f"CUDA Device Name: {torch.cuda.get_device_name(0)}")
-else:
-    print("CUDA is not available to PyTorch.")
